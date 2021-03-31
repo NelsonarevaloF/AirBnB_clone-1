248d222caa3af42f6a4a70c10d4b6e67f07c8867
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2021-03-31 03:20:27 +0000

    added: task 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- this script create a DB with the Specifications of holberton
+-- we are going to create DB to storage the objects of the console
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
